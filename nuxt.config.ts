73d32a041b7789edf473dbf1babb696008f37b66
Renjith S.S. <renjith4ss@gmail.com>
2025-01-12 21:54:46 +0530

    Update project name and title for Nuxt.js application

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Nuxt App',
+      title: 'SG Admin',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
